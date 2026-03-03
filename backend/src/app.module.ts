857425e35df89038c959542fe4d6c217c5027bd7
KK <2693289352@qq.com>
2026-03-03 23:38:08 +0800

    feat: 升级依赖库（9 个）
    新增包：node_modules/@graphql-toolkit/file-uploader/node_modules/file-uploader
    
    详细信息：
    • 更新了 eslint/eslintrc 依赖到 3.3.4
    • 更新了 ansi-regex 依赖到 ^6.2.2
    • 更新了 brat-expansion 依赖到 ^2.0.2
    • 等等，其他全部都是依赖库的更新

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
+// backend/src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
+import { UserModule } from './user/user.module'; // <- 相对路径必须正确
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
