f5b42f281f2680dce65858a56b76ddd99d4ea366
KK <2693289352@qq.com>
2026-03-03 23:38:08 +0800

    feat: 升级依赖库（9 个）
    新增包：node_modules/@graphql-toolkit/file-uploader/node_modules/file-uploader
    
    详细信息：
    • 更新了 eslint/eslintrc 依赖到 3.3.4
    • 更新了 ansi-regex 依赖到 ^6.2.2
    • 更新了 brat-expansion 依赖到 ^2.0.2
    • 等等，其他全部都是依赖库的更新

---
+++ backend/src/user/user.module.ts
@@ -0,0 +1,13 @@
+// backend/src/user/user.module.ts
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module'; // <- 相对路径必须正确
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [UserController],
+  providers: [UserService],
+  
+})
+export class UserModule {}
