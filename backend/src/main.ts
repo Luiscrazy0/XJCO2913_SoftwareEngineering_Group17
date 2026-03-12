edbbb66fdaa04aac5a3b6154b204d9a442d33b51
KK <2693289352@qq.com>
2026-03-12 17:13:06 +0800

    feat(auth): 增加登录及注册验证
    · 增加 ValidationPipe, 默认开启强校验
    · 登录及注册接口改用 DTO 验证
    · 导入 class-validator 类库并安装
    
    详细更新:
    * 添加 class-validator 支持
    * 强校验多余字段
    * 使用 DTO 验证数据

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,20 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,              // 自动删除多余字段
+        forbidNonWhitelisted: true,   // 多余字段直接报错
+        transform: true               // 自动类型转换
+      })
+    );
+
+    await app.listen(3000);
+  }
 }
 
 bootstrap();
